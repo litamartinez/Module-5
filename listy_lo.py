1da651fe17740aba178ebbc7e105905a24791db7
litamartinez <58664335+litamartinez@users.noreply.github.com>
2022-02-17 22:30:04 -0800

    adding breakfast

--- listy_lo.py
+++ listy_lo.py
@@ -1,1 +1,23 @@
+if __name__ == "__main__":
 
+    food = ['rice', 'beans']
+    food.append('broccoli')
+    
+#time to mess this list up!
+    carbs = ['bread', 'pizza']
+
+    food.extend(carbs)
+    
+    print(food)
+
+#now to slice...
+first2items = food[0:2]
+
+print(first2items)
+
+#crying breakfast friends below
+bad_breakfast = "eggs fruit orange_juice"
+
+breakfast = bad_breakfast.split()
+
+print(breakfast)
