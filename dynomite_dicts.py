06412f78a2c900060c985b906f9556a40165a38f
litamartinez <58664335+litamartinez@users.noreply.github.com>
2022-02-18 17:40:52 -0800

    a bunch of changes

--- dynomite_dicts.py
+++ dynomite_dicts.py
@@ -1,1 +1,15 @@
 
+#empty pokedex dictionary
+pokedex = {}
+
+#not-so empty pokedex
+
+pokedex = {
+    'Venosaur': ['Grass', 'Poison']
+    'Charizard': ['Fire', 'Flying']
+    'Blastoise': ['Water']
+}
+
+#sayonara, Blastoise!
+
+
--- listy_lo.py
+++ listy_lo.py
@@ -3,21 +3,24 @@ if __name__ == "__main__":
     food = ['rice', 'beans']
     food.append('broccoli')
     
-#time to mess this list up!
+#time to mess this list up! THIS ISS DONE
     carbs = ['bread', 'pizza']
 
     food.extend(carbs)
     
     print(food)
 
-#now to slice...
+#now to slice... THIS IS DONE
 first2items = food[0:2]
 
 print(first2items)
 
-#crying breakfast friends below
+#let's make some breakfast! THIS IS DONE
 bad_breakfast = "eggs fruit orange_juice"
 
 breakfast = bad_breakfast.split()
 
 print(breakfast)
+
+#using len on breakfast THISS IS DONE
+print(len(breakfast))
