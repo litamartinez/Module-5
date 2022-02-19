fe1cd5754874c75a8b06a0ecf06243ed6e98583f
litamartinez <58664335+litamartinez@users.noreply.github.com>
2022-02-19 13:05:42 -0800

    updates to list & dictionary

--- listy_lo.py
+++ listy_lo.py
@@ -3,24 +3,24 @@ if __name__ == "__main__":
     food = ['rice', 'beans']
     food.append('broccoli')
     
-#time to mess this list up! THIS ISS DONE
+#time to mess up this list!
     carbs = ['bread', 'pizza']
 
     food.extend(carbs)
     
     print(food)
 
-#now to slice... THIS IS DONE
+#now to slice... 
 first2items = food[0:2]
 
 print(first2items)
 
-#let's make some breakfast! THIS IS DONE
+#let's make some breakfast! 
 bad_breakfast = "eggs fruit orange_juice"
 
 breakfast = bad_breakfast.split()
 
 print(breakfast)
 
-#using len on breakfast THISS IS DONE
+#using len on breakfast 
 print(len(breakfast))
