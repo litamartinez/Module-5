7424804080dd8d5873c114b66f43801fa2b67e7d
litamartinez <58664335+litamartinez@users.noreply.github.com>
2022-02-19 14:12:29 -0800

    whew, all done

--- listy_lo.py
+++ listy_lo.py
@@ -24,3 +24,34 @@ print(breakfast)
 
 #using len on breakfast 
 print(len(breakfast))
+
+#prompt for floating point value -_-
+
+numbers=[]
+numbers_input=0
+
+while numbers_input!="stop":
+    #try:
+        numbers_input=input("Enter a floating value. Otherwise, type STOP.")
+        if numbers_input=="stop":
+
+            print("All done!")
+            print(f"You entered:{numbers}")
+    #except:
+    #        print("Oops, try again with a floating value (e.g. a real number with a decimal point):")
+            break
+    
+        numbers.append(float(numbers_input))
+    
+print(numbers)
+
+#second part with results
+numbers_average = sum(numbers) / len(numbers)
+
+print("In case you were wondering...")
+
+print(f"The minimum value you entered is: {min(numbers)}")
+
+print(f"The maximum value you entered is: {max(numbers)}")
+
+print(f"The average value of what you entered is: {numbers_average}")
